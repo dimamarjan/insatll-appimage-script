0b588eeb14f04b64e43e0bdf9aa3402d44178fd2
Dmitriy Marianchenko <dima.marjan@gmail.com>
2024-06-24 22:52:17 +0300

    Update install_appimage.sh

--- install_appimage.sh
+++ install_appimage.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 if [[ $# -lt 2 || $# -gt 3 ]]; then
-    echo "Usage: $0 <path_to_appimage> <path_to_icon> [name]"
+    echo "Usage: $0 <path_to_appimage_or_exe> <path_to_icon> [name]"
     exit 1
 fi
 
-APPIMAGE_FILE="$1"
+APP_FILE="$1"
 ICON_FILE="$2"
-APP_NAME="${3:-$(basename "$APPIMAGE_FILE" .AppImage)}"
+APP_NAME="${3:-$(basename "$APP_FILE" .AppImage)}"
+APP_NAME="${APP_NAME%%.exe}"
 ICON_EXT="${ICON_FILE##*.}"
 
-if [[ ! -f "$APPIMAGE_FILE" ]]; then
-    echo "AppImage file not found: $APPIMAGE_FILE"
+if [[ ! -f "$APP_FILE" ]]; then
+    echo "Application file not found: $APP_FILE"
     exit 1
 fi
 
@@ -20,7 +21,7 @@ if [[ ! -f "$ICON_FILE" ]]; then
     exit 1
 fi
 
-SYMLINK="/usr/local/bin/$APP_NAME.appimage"
+SYMLINK="/usr/local/bin/$APP_NAME"
 
 ICON_DIR="/usr/share/icons/hicolor/512x512/apps"
 sudo mkdir -p "$ICON_DIR"
@@ -28,13 +29,18 @@ sudo mkdir -p "$ICON_DIR"
 ICON_DEST="$ICON_DIR/$APP_NAME.png"
 sudo cp "$ICON_FILE" "$ICON_DEST"
 
-sudo ln -sf "$(realpath "$APPIMAGE_FILE")" "$SYMLINK"
-echo "Symbolic link updated: $SYMLINK -> $APPIMAGE_FILE"
+sudo ln -sf "$(realpath "$APP_FILE")" "$SYMLINK"
+echo "Symbolic link updated: $SYMLINK -> $APP_FILE"
 
 DESKTOP_FILE_PATH="$HOME/.local/share/applications/$APP_NAME.desktop"
+EXEC_CMD="$SYMLINK --no-sandbox %U"
+if [[ "$APP_FILE" == *.exe ]]; then
+    EXEC_CMD="wine $SYMLINK"
+fi
+
 echo "[Desktop Entry]
 Name=$APP_NAME
-Exec=$SYMLINK --no-sandbox %U
+Exec=$EXEC_CMD
 Terminal=false
 Type=Application
 Icon=$ICON_DIR/$APP_NAME.$ICON_EXT
@@ -47,4 +53,3 @@ echo "Desktop file created: $DESKTOP_FILE_PATH"
 
 update-desktop-database "$HOME/.local/share/applications"
 echo "Desktop database updated."
-
